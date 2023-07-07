b8537392161e15c06d15eb3dcfed163a42e3543d
CHIOE999 <chioe_lovely@163.com>
2023-07-07 17:32:54 +0800

    更新

---
+++ .history/src/views/equipment/ledger/list/weaning/index_20230706172047.vue
@@ -0,0 +1,296 @@
+<template>
+    <div class="ele-body">
+        <a-card class="cardBody">
+            <h5 style="text-align: center">
+                <span>撤机申请单</span>
+            </h5>
+            <div class="stepBox">
+                <a-steps :current=" 0 ">
+                    <a-step sub-title="填写撤机流程单">
+                        <template #description> </template>
+                    </a-step>
+                    <a-step title="" sub-title="等待审批" />
+                    <a-step sub-title="完成" />
+                </a-steps>
+            </div>
+            <a-card style="width: 90%;margin:0 auto;margin-top: 50px;">
+                <template #title>
+                    <div style="text-align: center;color:orangered;font-weight: 300;">
+                        请确认，设备布机信息
+                    </div>
+                </template>
+                <div style="color:gray;" id="deviceInfo">
+                    <div class="infoItem">
+                        <div>
+                            设备编号：{{  orderInfo?.device_code }}
+                        </div>
+                        <div>
+                            名称：{{  orderInfo?.deviceInfos ?.name}}
+                        </div>
+                        <div>
+                            类型：{{  orderInfo?.deviceInfos ?.category}}
+                        </div>
+                        <div>
+                            型号 ：{{  orderInfo?.deviceInfos ?.type}}
+                        </div>
+                        <div>
+                            状态：{{  orderInfo?.deviceInfos ?.name}}
+                        </div>
+                        <div>
+                            设备激活时间：{{  orderInfo?.deviceInfos ?.name}}
+                        </div>
+                        <div>
+                            设备激活账户：{{  orderInfo?.deviceInfos ?.name}}
+                        </div>
+                    </div>
+                    <a-divider />
+                    <div class="storeInfo">
+                        <div>
+                            门店名称：{{  orderInfo?.store ?.name}}
+                        </div>
+                        <div>
+                            门店业态：{{  orderInfo?.store ?.service}}
+                        </div>
+                        <div>
+                            门店地址：{{  orderInfo?.store ?.province}}{{  orderInfo?.store ?.city}}{{  orderInfo?.store ?.area}}{{  orderInfo?.store ?.address_detail}}
+                        </div>
+                    </div>
+                    <a-divider />
+                    <div class="channelInfo">
+                        <div>
+                            客户名称：{{  orderInfo?.user ?.name}}
+                        </div>
+                        <div>
+                            联系人：{{  orderInfo?.user ?.contact}}
+                        </div>
+                        <div>
+                            联系方式：{{  orderInfo?.user ?.phone}}
+                        </div>
+                    </div>
+                    <div class="channelInfo">
+                        <div>
+                            渠道名称：{{  orderInfo?.channel ?.name}}
+                        </div>
+                        <div>
+                            联系人：{{  orderInfo?.channel ?.contact}}
+                        </div>
+                        <div>
+                            联系方式：{{  orderInfo?.channel ?.phone}}
+                        </div>
+                    </div>
+                </div>
+            </a-card>
+            <div style="text-align: right; width: 80%; margin: 0 auto; margin-top: 30px">
+                <a-button style="margin-right: 20px" @click="cancle">取消</a-button>
+                <a-button type="primary" @click="confirm">提交</a-button>
+            </div>
+        </a-card>
+    </div>
+</template>
+<script>
+import { defineComponent, ref ,reactive} from 'vue';
+import {
+    ContactsOutlined,
+    FormOutlined,
+    DeleteOutlined,
+    PlayCircleOutlined
+} from '@ant-design/icons-vue';
+import { useRouter } from 'vue-router';
+import { toDateString } from 'ele-admin-pro';
+import {  notification } from 'ant-design-vue/es';
+import { finishPageTab } from '@/utils/page-tab-util';
+import {addDeviceOrder,withdrawalOrder,approvalDetail} from '@/api/equipment/ledger/workOrder'
+export default defineComponent({
+    name: 'Deployment',
+    components: {
+        ContactsOutlined,
+        FormOutlined,
+        DeleteOutlined,
+        PlayCircleOutlined
+    },
+    setup() {
+        const { currentRoute ,push} = useRouter();
+        let step=ref()
+        let orderInfo=reactive({})
+        let orderId = currentRoute.value.query.orderId;
+        const getDetail=()=>{
+            approvalDetail(orderId).then((res)=>{
+                if(res.code==0){
+                    Object.assign(orderInfo,res.data)
+                    orderInfo.operate_at=toDateString(orderInfo.operate_at)
+                    orderInfo.created_at=toDateString(orderInfo.created_at)
+                    if(orderInfo?.status=='pending'){
+                        step.value=1
+                    }else{
+                        step.value=2
+                    }
+                }
+                // console.log(orderInfo)
+                
+            })
+        }
+        if(orderId){
+            getDetail()
+        }
+        
+        const confirm=()=>{
+            // console.log(orderInfo)
+            withdrawalOrder(currentRoute.value.query.id).then((res)=>{
+                if(res.code==0){
+                    notification.success({
+                        message: res.message
+                    });
+                }
+                finishPageTab()
+                push({
+                    path:'/equipment/ledger/list'
+                })
+                // console.log(res)
+            })
+        }
+
+        const cancle=()=>{
+            finishPageTab()
+            push({
+                path:'/equipment/ledger/list'
+            })
+        }
+        return {
+            confirm,
+            orderId,
+            getDetail,
+            step,
+            orderInfo,
+            cancle,
+        };
+    }
+});
+</script>
+<style lang="less" scoped>
+.cardBody {
+    // text-align: center;
+    #orderInfo{
+        .infoItem{
+            display: flex;
+            line-height: 40px;
+            div{
+                width: 50%;
+            }
+        }
+    }
+    #deviceInfo{
+        .infoItem{
+            display: flex;
+            line-height: 40px;
+            margin-bottom: 10px;
+            flex-wrap: wrap;
+            div{
+                min-width: 20%;
+                margin-right: 10px;
+            }
+        }
+        .storeInfo{
+            display: flex;
+            line-height: 40px;
+            flex-wrap: wrap;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            div{
+                width: 50%;
+            }
+        }
+        .channelInfo{
+            display: flex;
+            line-height: 40px;
+            flex-wrap: wrap;
+            margin-top: 10px;
+            div{
+                width: 30%;
+            }
+        }
+    }
+    /deep/.ant-card-head{
+        background-color: #EDEDED;
+        font-size: 16px;
+        font-weight: normal;
+        color: gray;
+    }
+    .stepBox {
+        width: 70%;
+        margin: 0 auto;
+        margin-top: 20px;
+    }
+
+    .orderInfo {
+        width: 50%;
+        margin: 0 auto;
+        margin-top: 50px;
+
+        .deviceInfo {
+            h6 {
+                margin-bottom: 10px;
+            }
+
+            .deviceDes {
+                color: gray;
+
+                .desTop {
+                    display: flex;
+                    line-height: 36px;
+
+                    div {
+                        width: 50%;
+                    }
+                }
+
+                .desBottom {
+                    display: flex;
+                    line-height: 36px;
+
+                    div {
+                        width: 30%;
+                    }
+                }
+            }
+        }
+
+        .chooseStore {
+            h6 {
+                margin: 10px 0px;
+            }
+
+            div {
+                line-height: 36px;
+                margin-bottom: 10px;
+            }
+        }
+
+        .channelInfo {
+            margin: 10px 0px;
+
+            div {
+                line-height: 36px;
+                color: gray;
+            }
+        }
+
+        .userInfo {
+            margin: 10px 0px;
+
+            div {
+                line-height: 36px;
+                color: gray;
+            }
+        }
+
+        .storeInfo {
+            margin: 10px 0px;
+
+            div {
+                line-height: 36px;
+                color: gray;
+            }
+        }
+    }
+}
+</style>
---
+++ .history/src/views/equipment/statistics/index_20230706155207.vue
@@ -0,0 +1,194 @@
+<template>
+    <div class="ele-body">
+        <a-card style="margin-top: 10px;border-radius: 4px;">
+            <div id="proTable">
+                <ele-pro-table ref="tableRef" title="设备统计" :resizable="true" :bordered="true" :columnsFixed="true" :needPage="false"
+                    :columns="columns" :datasource="datasource" :scroll="{ x: 1000 }">
+                    <!-- 表头工具按钮 -->
+                    <!-- <template #toolkit>
+                        <a-button type="primary" @click="toAdd">新增产品信息</a-button>
+                    </template> -->
+                    <!-- 自定义列 -->
+                    <template #bodyCell="{ column, record }">
+                        <template v-if="column.dataIndex === 'mach_sts'">
+                            <div>
+                                <span v-if="record.iot_sts=='0'" style="color:gray">
+                                    <span class="iconfont">&#xec1e;</span>
+                                    离线
+                                </span>
+                                <span v-if="record.iot_sts=='1'" style="color:green"><span class="iconfont">&#xec1e;</span>在线</span>
+                                <span v-if="record.iot_sts=='2'" style="color:gray"><span class="iconfont">&#xec1e;</span>故障</span>
+                            </div>
+                            <!-- <div>
+                                <span v-if="record.mach_sts==0" style="color:gray"><span class="iconfont">&#xec1e;</span>待机</span>
+                                <span v-if="record.mach_sts==1" style="color:green"><span class="iconfont">&#xec1e;</span>运行</span>
+                                <span v-if="record.mach_sts==2" style="color:gray"><span class="iconfont">&#xec1e;</span>故障</span>
+                            </div> -->
+                        </template>
+                        <template v-else-if="column.dataIndex === 'username'">
+                            <span>{{ record?.user?.name }}</span>
+                        </template>
+                        <template v-else-if="column.dataIndex === 'storeName'">
+                            <span>{{ record?.store?.name }}</span>
+                        </template>
+                        <!-- 操作列 -->
+                        <template v-else-if="column.key === 'action'">
+                        </template>
+                    </template>
+                </ele-pro-table>
+            </div>
+            <div style="text-align:right;margin-top: 10px;">
+                <a-pagination size="small" :total="datasource.length" @change="changePage"
+                    :show-total="listCount => `共 ${ datasource.length } 条`" />
+            </div>
+        </a-card>
+    </div>
+</template>
+<script>
+import { defineComponent, ref ,reactive,computed} from 'vue';
+import {getDeviceState} from '@/api/equipment/statistics/index'
+export default defineComponent({
+    setup() {
+        let listCount=ref(0)
+        let pageData = reactive({
+            page: 1,
+            limit: 10
+        })
+        let datasource = ref([])
+         // 表格列配置
+        const columns = computed(() => {
+            return [
+                {
+                    key: 'index',
+                    width: 52,
+                    align: 'center',
+                    fixed: 'left',
+                    hideInSetting: true,
+                    customRender: ({ index }) => index + 1
+                },
+                {
+                    title: '设备编号',
+                    dataIndex: 'mach_no',
+                    width: 190,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '门店名称',
+                    dataIndex: 'storeName',
+                    width: 300,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '客户名称',
+                    dataIndex: 'username',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '运行状态',
+                    dataIndex: 'mach_sts',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '当日开机累计时长',
+                    dataIndex: 'day_run_time',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '当日炒制累计时长',
+                    dataIndex: 'day_work_time',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '当日炒制总份数',
+                    dataIndex: 'day_work_time',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '激活开机累计时长',
+                    dataIndex: 'act_run_time',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '激活炒制累计时长',
+                    dataIndex: 'act_work_time',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '激活炒制总份数',
+                    dataIndex: 'act_work_no',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '总开机累计时长',
+                    dataIndex: 'total_run_time',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '更新时间',
+                    dataIndex: 'shopAddress',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                {
+                    title: '总炒制总份数',
+                    dataIndex: 'total_work_no',
+                    width: 160,
+                    minWidth: 100,
+                    align: 'center'
+                },
+                // {
+                //     title: '操作',
+                //     key: 'action',
+                //     width: 160,
+                //     align: 'center',
+                //     hideInSetting: true,
+                //     fixed: 'right'
+                // }
+            ];
+        });
+
+        const getList=()=>{
+            getDeviceState().then((res)=>{
+                if(res.code==0){
+                    datasource.value=res.data
+                }
+            })
+        }
+        getList()
+        const changePage = (page) => {
+            pageData.page = page
+            getList()
+        }
+        
+        return{
+            listCount,
+            changePage,
+            pageData,
+            columns,
+            datasource,
+            getList,
+        }
+    }
+})
+</script>
